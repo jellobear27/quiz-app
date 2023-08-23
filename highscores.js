ea48b7cacb0405445cc0fc7fa7ebc0c27e3b738c
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-08-23 18:33:29 -0400

    made changes to get score to work

--- highscores.js
+++ highscores.js
@@ -1,7 +1,10 @@
 const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
+highScores.sort((a, b) => b.score - a.score);
+
 highScoresList.innerHTML =
+
 highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
