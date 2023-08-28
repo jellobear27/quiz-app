ffe1ab55b28404a01897577aa76d9ff080609608
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-08-27 21:53:27 -0400

    made changes to deploy

--- game.js
+++ game.js
@@ -83,7 +83,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", timeRemaining);  //sets most recent score to time remaining
 
-    return window.location.assign("/end.html");
+    return window.location.assign("end.html");
   }
 
   questionCounter++;
